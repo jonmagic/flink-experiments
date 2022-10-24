2a70ee495a25ff2835c024b161136e1769d65031
Jonathan Hoyt <jonmagic@gmail.com>
2022-10-24 13:35:45 -0700

    Update KafkaExample program

--- src/main/java/com/jonmagic/flink/experiments/KafkaExample.java
+++ src/main/java/com/jonmagic/flink/experiments/KafkaExample.java
@@ -8,27 +8,27 @@ public class KafkaExample {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         StreamTableEnvironment tEnv = StreamTableEnvironment.create(env);
         tEnv.executeSql(
-                 "CREATE TABLE simple_test (" +
-                 "  uid BIGINT," +
-                 "  name STRING," +
-                 "  category_type INT," +
-                 "  content BINARY," +
-                 "  price DOUBLE," +
-                 "  value_map map<BIGINT, row<v1 BIGINT, v2 INT>>," +
-                 "  value_arr array<row<v1 BIGINT, v2 INT>>," +
-                 "  corpus_int INT," +
-                 "  corpus_str STRING" +
-                 ") WITH (" +
-                 "  'connector' = 'kafka'," +
-                 "  'topic' = 'user_behavior'," +
-                 "  'properties.bootstrap.servers' = 'localhost:9092'," +
-                 "  'properties.group.id' = 'testGroup'," +
-                 "  'scan.startup.mode' = 'earliest-offset'," +
-                 "  'format' = 'protobuf'," +
-                 "  'protobuf.message-class-name' = 'com.jonmagic.flink.experiments.SimpleTest'," +
-                 "  'protobuf.ignore-parse-errors' = 'true'" +
-                 ")"
+                "CREATE TABLE simple_test (" +
+                "  uid BIGINT," +
+                "  name STRING," +
+                "  category_type INT," +
+                "  content BINARY," +
+                "  price DOUBLE," +
+                "  value_map map<BIGINT, row<v1 BIGINT, v2 INT>>," +
+                "  value_arr array<row<v1 BIGINT, v2 INT>>," +
+                "  corpus_int INT," +
+                "  corpus_str STRING" +
+                ") WITH (" +
+                "  'connector' = 'kafka'," +
+                "  'topic' = 'user_behavior'," +
+                "  'properties.bootstrap.servers' = 'localhost:9092'," +
+                "  'properties.group.id' = 'testGroup'," +
+                "  'scan.startup.mode' = 'earliest-offset'," +
+                "  'format' = 'protobuf'," +
+                "  'protobuf.message-class-name' = 'com.jonmagic.flink.experiments.SimpleTest'," +
+                "  'protobuf.ignore-parse-errors' = 'true'" +
+                ")"
         );
-        tEnv.executeSql("SELECT * FROM simple_test").print();
+        tEnv.executeSql("SELECT uid, price FROM simple_test").print();
     }
 }
